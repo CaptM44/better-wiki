64733c5f926d93c32a1ffb0c64756ec69b64383e
Matt <mmm211@zips.uakron.edu>
2018-05-11 21:47:51 -0400

    removed options

--- app/background.js
+++ app/background.js
@@ -1,13 +1,14 @@
 var useCss = true;
 
+//toggle injected css on and off
 chrome.browserAction.onClicked.addListener(function (tab) {
-    useCss = !useCss
+    useCss = !useCss;
     chrome.tabs.executeScript(tab.id, {
         code: `
             var useCss = ${useCss};
             var sheets = document.querySelectorAll('.injectedCss');
             
-            for (var i = 0; i< sheets.length; i++) { 
+            for (var i = 0; i < sheets.length; i++) { 
                 sheets[i].disabled = !useCss;
             }
         `
--- app/script.js
+++ app/script.js
@@ -3,32 +3,6 @@ loadCss('/styles/toc.css');
 loadCss('/styles/hidden.css');
 loadCss('/styles/markdown.css');
 
-loadOptions(function (options) {
-    if (options.fixedNav) {
-        var toc = document.getElementById('toc');
-        toc.style.position = 'fixed';
-        var prevY = 0;
-
-        document.addEventListener('scroll', function () {
-            var y = window.pageYOffset || document.documentElement.scrollTop;
-            delta = y - prevY;
-            prevY = y;
-
-            var top = parseFloat(window.getComputedStyle(toc).top);
-            top -= delta;
-
-            var maxTop = y > 160 ? 0 : 160;
-            top = top > maxTop ? maxTop : top;
-
-            var minTop = window.innerHeight - toc.clientHeight;
-            minTop = minTop > 0 ? 0 : minTop;
-            top = top < minTop ? minTop : top;
-
-            toc.style.top = top + 'px';
-        });
-    }
-})
-
 function loadCss(path) {
     var link = document.createElement("link");
     link.rel = 'stylesheet'
@@ -36,16 +10,8 @@ function loadCss(path) {
     link.href = chrome.extension.getURL(path);
     link.classList.add("injectedCss");
 
-    document.documentElement.appendChild(link)
+    document.documentElement.appendChild(link);
     setTimeout(function () {
-        document.documentElement.appendChild(link)
+        document.documentElement.appendChild(link);
     });
-}
-
-function loadOptions(cb) {
-    var defaultOptions = {
-        fixedNav: true,
-    }
-
-    chrome.storage.sync.get(defaultOptions, cb);
 }
